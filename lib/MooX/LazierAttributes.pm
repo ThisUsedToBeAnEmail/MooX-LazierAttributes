dca19aec3492d88d44969a47fd26196b5ca292ba
LNATION <thisusedtobeanemail@gmail.com>
2018-09-05 23:19:32 +0800

    all svg

--- lib/MooX/LazierAttributes.pm
+++ lib/MooX/LazierAttributes.pm
@@ -98,7 +98,7 @@ MooX::LazierAttributes - Lazier Attributes.
 =for html
 
 <a href="https://travis-ci.org/ThisUsedToBeAnEmail/MooX-LazierAttributes"><img src="https://travis-ci.org/ThisUsedToBeAnEmail/MooX-LazierAttributes.svg?branch=master" alt="Build Status"></a>
-<a href="https://coveralls.io/r/ThisUsedToBeAnEmail/MooX-LazierAttributes?branch=master"><img src="https://coveralls.io/repos/ThisUsedToBeAnEmail/MooX-LazierAttributes/badge.png?branch=master" alt="Coverage Status"></a>
+<a href="https://coveralls.io/r/ThisUsedToBeAnEmail/MooX-LazierAttributes?branch=master"><img src="https://coveralls.io/repos/ThisUsedToBeAnEmail/MooX-LazierAttributes/badge.svg?branch=master" alt="Coverage Status"></a>
 <a href="https://metacpan.org/pod/MooX-LazierAttributes"><img src="https://badge.fury.io/pl/MooX-LazierAttributes.svg" alt="CPAN version"></a>
 
 =head1 VERSION
